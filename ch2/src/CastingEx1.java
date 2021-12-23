3b06d5f23b4f57e0239984582a1e598266f7d4c5
g471000 <g471000@gmail.com>
2021-12-23 20:15:54 +0900

    print double and int after casting

---
+++ ch2/src/CastingEx1.java
@@ -0,0 +1,10 @@
+public class CastingEx1 {
+    public static void main(String[] args) {
+        double d = 87.6;
+        int score = (int) d;
+
+        System.out.println("score = " + score);
+        System.out.println("d = " + d);
+
+    }
+}
